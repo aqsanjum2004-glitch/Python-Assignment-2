7c0ff1c839891958ea26f3f6fae5d6357c8d8cee
aqsanjum2004-glitch <aqsanjum2004@gmail.com>
2026-02-22 16:33:51 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,52 @@
+
+# Temperature Converter - Menu Based Program
+
+
+while True:
+    print("\n===== TEMPERATURE CONVERTER =====")
+    print("1. Celsius to Fahrenheit")
+    print("2. Fahrenheit to Celsius")
+    print("3. Celsius to Kelvin")
+    print("4. Kelvin to Celsius")
+    print("5. Fahrenheit to Kelvin")
+    print("6. Kelvin to Fahrenheit")
+    print("7. Exit")
+
+    choice = int(input("Enter your choice (1-7): "))
+
+    if choice == 1:
+        c = float(input("Enter temperature in Celsius: "))
+        f = (c * 9/5) + 32
+        print("Temperature in Fahrenheit:", round(f, 2))
+
+    elif choice == 2:
+        f = float(input("Enter temperature in Fahrenheit: "))
+        c = (f - 32) * 5/9
+        print("Temperature in Celsius:", round(c, 2))
+
+    elif choice == 3:
+        c = float(input("Enter temperature in Celsius: "))
+        k = c + 273.15
+        print("Temperature in Kelvin:", round(k, 2))
+
+    elif choice == 4:
+        k = float(input("Enter temperature in Kelvin: "))
+        c = k - 273.15
+        print("Temperature in Celsius:", round(c, 2))
+
+    elif choice == 5:
+        f = float(input("Enter temperature in Fahrenheit: "))
+        k = (f - 32) * 5/9 + 273.15
+        print("Temperature in Kelvin:", round(k, 2))
+
+    elif choice == 6:
+        k = float(input("Enter temperature in Kelvin: "))
+        f = (k - 273.15) * 9/5 + 32
+        print("Temperature in Fahrenheit:", round(f, 2))
+
+    elif choice == 7:
+        print("Exiting program... Thank you!")
+        break
+
+    else:
+        print("Invalid choice! Please select between 1 and 7.")
